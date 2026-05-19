681684d1bb51d49dc33c08687cc509c27d81d18f
Anvesh <anveshsahu15@gmail.com>
2026-05-19 10:20:56 +0530

    [Anvesh] : collaboration module with team feature integration

---
+++ src/app/features/collaboration/collaboration-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CollaborationRoutingModule } from './collaboration-routing-module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, CollaborationRoutingModule],
+})
+export class CollaborationModule {}
